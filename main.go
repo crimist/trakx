0f59c249d282bc0076185da0e4607d2ea0f8e505
Syc0x00 <syc0x00@gmail.com>
2019-06-19 17:34:01 -0700

    New ui

--- main.go
+++ main.go
@@ -11,17 +11,39 @@ import (
 	"github.com/Syc0x00/Trakx/tracker"
 )
 
-const indexStyle = "<style> body { background-color: black; font-family: arial; color: white; font-size: 25px; } </style>"
-const dmcaStyle = "<style> body { background-color: black; font-family: arial; color: white; font-size: 200px; } </style>"
+const trackerBase = "http://nibba.trade:1337"
+const netdataBase = "https://nibba.trade/netdata"
+
+const indexHTML = `
+<head>
+	<title>Trakx</title>
+</head>
+<style>
+body {
+	background-color: black;
+	font-family: arial;
+	color: #9b9ea3;
+	font-size: 25px;
+	text-align: center;
+	align-items: center;
+	display: flex;
+	justify-content: center;
+}
+</style>
+<div>
+	<p>Trakx is an open p2p tracker. Feel free to use it :)</p>
+	<p>Add <span style="background-color: #1dc135; color: black;">` + trackerBase + `/announce</span></p>
+	<embed src="` + netdataBase + `/api/v1/badge.svg?chart=go_expvar_Trakx.hits_sec&alarm=trakx_hits&refresh=auto" type="image/svg+xml" height="20"/>
+	<embed src="` + netdataBase + `/api/v1/badge.svg?chart=go_expvar_Trakx.hits_sec&alarm=trakx_hits_5min&refresh=auto" type="image/svg+xml" height="20"/>
+	<embed src="` + netdataBase + `/api/v1/badge.svg?chart=go_expvar_Trakx.hits_sec&alarm=trakx_hits_1hour&refresh=auto" type="image/svg+xml" height="20"/>
+	<embed src="` + netdataBase + `/api/v1/badge.svg?chart=go_expvar_Trakx.errors_per_second&alarm=trakx_errors&refresh=auto" type="image/svg+xml" height="20"/>
+	<p>Discord: <3#1527 / Email: tracker@nibba.trade</p>
+	<a href='/dmca'>DMCA?</a>
+</div>
+`
 
 func index(w http.ResponseWriter, r *http.Request) {
-	resp := indexStyle
-	resp += "<p>This is an open p2p tracker. Feel free to use it :)</p>"
-	resp += "<p>http://nibba.trade:1337/announce</p>"
-	resp += "<p>Message me if you've got issues. Discord: <3#1527 / Email: tracker@nibba.trade</p>"
-	resp += "<a href='/dmca'>DMCA?</a>"
-
-	fmt.Fprintf(w, resp)
+	fmt.Fprintf(w, indexHTML)
 }
 
 func dmca(w http.ResponseWriter, r *http.Request) {
