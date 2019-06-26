4954b2ee907059df6fcb030198930cb4bbde2048
Syc0x00 <syc0x00@gmail.com>
2019-06-25 17:37:23 -0700

    init maps

--- tracker/shared/expvar.go
+++ tracker/shared/expvar.go
@@ -11,3 +11,12 @@ var (
 	ExpvarIPs     map[string]bool
 	ExpvarPeers   map[PeerID]bool
 )
+
+// !x
+func initExpvar() {
+	// Might as well alloc capcity at start
+	ExpvarSeeds = make(map[string]bool, 50000)
+	ExpvarLeeches = make(map[string]bool, 50000)
+	ExpvarIPs = make(map[string]bool, 30000)
+	ExpvarPeers = make(map[PeerID]bool, 100000)
+}
