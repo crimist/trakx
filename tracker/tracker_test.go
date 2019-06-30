d5c27791742e7cb9cf90632b25e3d3531e06cae9
Syc0x00 <syc0x00@gmail.com>
2019-06-29 18:48:59 -0700

    remove useless

--- tracker/tracker_test.go
+++ tracker/tracker_test.go
@@ -3,7 +3,6 @@ package tracker_test
 import (
 	"bytes"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"testing"
 
@@ -11,23 +10,6 @@ import (
 )
 
 var client = &http.Client{}
-
-const (
-	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+[{]}\\|\":;<,>.?'/~`"
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-)
-
-func randStr(n int) string {
-	b := make([]byte, n)
-	for i := 0; i < n; {
-		if idx := int(rand.Int63() & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i++
-		}
-	}
-	return string(b)
-}
 
 func TestAnnounce(t *testing.T) {
 	req, err := http.NewRequest("GET", "http://127.0.0.1:1337/announce", nil)
