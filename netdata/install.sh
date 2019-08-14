0b425fe45ceb66c1e02a37bd21efc37ef2eeb4d1
Syc0x00 <syc0x00@gmail.com>
2019-08-14 12:26:20 -0700

    fix netdata restart

--- netdata/install.sh
+++ netdata/install.sh
@@ -1,4 +1,6 @@
 echo "Make sure go_expvar = yes in python.d.conf"
+
 cp expvar.conf /etc/netdata/python.d/go_expvar.conf
 cp alarm.conf /etc/netdata/health.d/trakx.conf
-netdata
+
+service netdata restart
