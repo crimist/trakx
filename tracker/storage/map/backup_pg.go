38f512c8f3563e0597ef3bfbba09302af6138667
crimist <syc0x00@gmail.com>
2020-01-14 00:38:04 -0800

    tune timings

--- tracker/storage/map/backup_pg.go
+++ tracker/storage/map/backup_pg.go
@@ -26,7 +26,7 @@ const (
 	backupRecentWindow = 10 * time.Minute
 
 	// Time to wait for if backup is older than backupRecentWindow
-	backupRecentWait = 7 * time.Second
+	backupRecentWait = 8 * time.Second
 )
 
 type PgBackup struct {
